dd0ad5375447b4fd5ca5cbde458bad121d7614c2
Loger <kidd415055@gmail.com>
2026-04-29 08:08:30 +0800

    refactor(route): predictions page uses RecommendationGrid + SortToggle

--- src/routes/$lang/predictions/route.tsx
+++ src/routes/$lang/predictions/route.tsx
@@ -1,18 +1,23 @@
+// src/routes/$lang/predictions/route.tsx
 import { createFileRoute } from '@tanstack/react-router'
 import { DateScrollBar } from '@/components/predictions/DateScrollBar'
-import { GameGrid } from '@/components/predictions/GameGrid'
-import { useDailyPredictions } from '@/hooks/predictions/useDailyPredictions'
+import { SortToggle } from '@/components/predictions/SortToggle'
+import { RecommendationGrid } from '@/components/predictions/RecommendationGrid'
+import { useDailyRecommendations } from '@/hooks/predictions/useDailyRecommendations'
 
 export const Route = createFileRoute('/$lang/predictions')({
   component: PredictionsPage,
 })
 
 function PredictionsPage() {
-  const query = useDailyPredictions()
+  const query = useDailyRecommendations()
   return (
     <div className="space-y-4">
       <DateScrollBar />
-      <GameGrid {...query} />
+      <div className="flex items-center justify-end">
+        <SortToggle />
+      </div>
+      <RecommendationGrid {...query} />
     </div>
   )
 }
